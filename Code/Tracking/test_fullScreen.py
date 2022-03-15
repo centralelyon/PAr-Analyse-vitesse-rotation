2194a1c515c813f65cc0911e6f5006ee881a168f
Baptiste Perreyon <baptiste.perreyon@ecl20.ec-lyon.fr>
2022-03-15 13:04:27 +0100

    Détection bord billard

--- Code/Tracking/test_fullScreen.py
+++ Code/Tracking/test_fullScreen.py
@@ -10,15 +10,26 @@ Created on Tue Mar  8 15:04:32 2022
 import cv2
 import numpy as np
 import screeninfo
+import copy
 
+def onClick(event,x,y,flags,params):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        print(x,y)
+        img=image.copy()
+        cv2.circle(img,(x,y),2,(255,0,0),-1)
+        cv2.putText(img,str(x)+"-"+str(y),(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.75,(0,0,255),2)
+        cv2.imshow(window_name,img)
 
 # get the size of the screen
 screen = screeninfo.get_monitors()[0] #remplacer 0 par le numéro de l'écran si plusieurs screens
 width, height = screen.width, screen.height
 
+camera = cv2.VideoCapture(0)
+o,image = camera.read()
+
 
 # create image
-image = np.ones((height, width, 3), dtype=np.float32)
+#image = np.ones((height, width, 3), dtype=np.float32)
 image[:10, :10] = 0  # black at top-left corner
 image[height - 10:, :10] = [1, 0, 0]  # blue at bottom-left
 image[:10, width - 10:] = [0, 1, 0]  # green at top-right
@@ -31,4 +42,11 @@ window_name = 'projector'
 cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
 cv2.moveWindow(window_name, screen.x - 1, screen.y - 1)
 cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+cv2.setWindowProperty(window_name, cv2.WND_PROP_TOPMOST, 1)
+cv2.setMouseCallback(window_name,onClick)
 cv2.imshow(window_name, image)
+
+while (True):
+    k=cv2.waitKey(1)
+    if k == ord('q'):
+        break
